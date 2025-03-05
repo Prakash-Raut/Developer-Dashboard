b4a2f5e33cedcfe464f55877582c8baee76045a8
Prakash Raut <prakash_raut@outlook.com>
2025-03-05 12:00:16 +0530

    add winston logger configuration and integrate with package.json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { app } from "./app";
 import { Config } from "./config/env";
+import { logger } from "./config/logger";
 
 const startServer = async () => {
 	try {
 		const PORT = Config.PORT;
 		app.listen(PORT, () => {
-			console.log(`Server is running on port ${PORT}`);
+			logger.info("Server is running on port", { port: PORT });
 		});
 	} catch (error) {
-		console.error(error);
+		logger.error("Error starting server", { error });
 		process.exit(1);
 	}
 };
