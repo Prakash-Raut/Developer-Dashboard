ae8ebdfbe7090ccf6ac992271ab16fa2021a8f6b
Prakash Raut <prakash_raut@outlook.com>
2025-03-05 12:02:58 +0530

    add global error handler and integrate uuid for error tracking

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { globalErrorHandler } from "./middleware/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
+
+app.use(globalErrorHandler);
 
 export { app };
